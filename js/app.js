05be4de9574fb188e8440be70e66842623e6e8f5
Jean <jean.soro007@gmail.com>
2019-10-22 13:51:51 -0400

    Create add to cart function

--- js/app.js
+++ js/app.js
@@ -30,12 +30,68 @@
   cartBtn.forEach(function (singleBtn) {
     singleBtn.addEventListener('click', function (e) {
       if (e.target.parentElement.classList.contains('store-item-icon')) {
+
         let fullPath = e.target.parentElement.previousElementSibling.src;
-
         let position = fullPath.indexOf('img') + 3; //+ 3 to remove 'img' from path
-
         let partialPosition = fullPath.slice(position)
-        console.log(partialPosition)
+
+        const item = {};
+        item.img = `img-cart${partialPosition}`;
+
+        let name = e.target.parentElement.parentElement.nextElementSibling.children[0].children[0].textContent;
+        item.name = name;
+
+
+
+        let price = e.target.parentElement.parentElement.nextElementSibling.children[0].children[1].textContent;
+        let finalPrice = price.slice(1).trim();
+        item.price = finalPrice;
+
+
+
+
+
+        let cartItem = document.createElement('div');
+        cartItem.classList.add('cart-item', 'd-flex', 'justify-content-between', 'text-capitalize', 'my-3');
+
+        cartItem.innerHTML = `
+       
+          <img src="${item.img}" class="img-fluid rounded-circle" id="item-img" alt="" srcset="">
+          <div class="item-text">
+            <p id="cart-item-title" class="font-weight-bold mb-0">
+              ${item.name}
+            </p>
+            <span>$</span>
+            <span id="cart-item-price" class="cart-item-price mb-0">$ ${item.price}</span>
+          </div>
+          <a href="#" id="cart-item-remove" class="cart-item-remove">
+            <i class="fas fa-trash"></i>
+          </a>
+        
+       
+        `;
+
+        //select cart
+        const cart = document.getElementById('cart');
+        const total = document.querySelector('.cart-total-container');
+
+        cart.insertBefore(cartItem, total);
+        alert('added');
+
+
+
+
+
+
+
+
+        // console.log(finalPrice)
+        console.log(item)
+
+
+
+
+
       }
 
 
