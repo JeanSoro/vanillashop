e06dd1fabad06093926428621e2829d0b52c7ee2
Jean <jean.soro007@gmail.com>
2019-10-22 13:10:40 -0400

    Create cart toggle function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,49 @@
-document.getElementById("cart-info").addEventListener("click", function() {
-  const cart = document.getElementById("cart");
-  cart.classList.toggle("show-cart");
-  console.log(cart);
-});
+// document.getElementById("cart-info").addEventListener("click", function() {
+//   const cart = document.getElementById("cart");
+//   cart.classList.toggle("show-cart");
+//   console.log(cart);
+// });
+
+
+//Show cart
+
+(function () {
+
+  const cartInfo = document.getElementById("cart-info");
+  const cart = document.getElementById('cart');
+
+
+  cartInfo.addEventListener('click', function () {
+    cart.classList.toggle('show-cart')
+  })
+
+})();
+
+
+//add item to cart
+
+(function () {
+
+
+  const cartBtn = document.querySelectorAll('.store-item-icon');
+
+  cartBtn.forEach(function (singleBtn) {
+    singleBtn.addEventListener('click', function (e) {
+      if (e.target.parentElement.classList.contains('store-item-icon')) {
+        let fullPath = e.target.parentElement.previousElementSibling.src;
+
+        let position = fullPath.indexOf('img') + 3; //+ 3 to remove 'img' from path
+
+        let partialPosition = fullPath.slice(position)
+        console.log(partialPosition)
+      }
+
+
+    })
+  })
+
+
+
+
+
+})()
